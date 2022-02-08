079c840887cd745d2169bd59e2c8d62fe1897a2e
shk <kim141312@naver.com>
2022-02-08 17:41:40 +0900

    BackUp 20220208 - RP에 배경색상 추가

--- src/com/aroundThirty/View/ReportPage.java
+++ src/com/aroundThirty/View/ReportPage.java
@@ -2,16 +2,18 @@ package com.aroundThirty.View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.Serial;
 import java.util.ArrayList;
 
 import static com.aroundThirty.Resource.FR.*;
 
 
 public class ReportPage extends JPanel {
+    @Serial
     private static final long serialVersionUID = 1L;
-    public static ArrayList<JPanel> paneList = new ArrayList<JPanel>();
-    public static ArrayList<JButton> btnList = new ArrayList<JButton>();
-    public static ArrayList<JLabel> lblList = new ArrayList<JLabel>();
+    public static ArrayList<JPanel> paneList = new ArrayList<>();
+    public static ArrayList<JButton> btnList = new ArrayList<>();
+    public static ArrayList<JLabel> lblList = new ArrayList<>();
     PagingBtn pagingBtn = new PagingBtn();
     JPanel centerPanel = new JPanel(new GridLayout(SIZE_ROW, SIZE_COL));    // SIZE_ROW, SIZE_COL로 행열 지정
     JScrollPane jScrollPane = new JScrollPane(centerPanel);
@@ -21,10 +23,12 @@ public class ReportPage extends JPanel {
     }
 
     public ReportPage() {
-        add(BorderLayout.CENTER,jScrollPane);
-        jScrollPane.setPreferredSize(new Dimension(800, 700));
+        pagingBtn.setBackground(pastelYellow);
+        add(BorderLayout.CENTER, jScrollPane);
+        jScrollPane.setPreferredSize(new Dimension(820, 650));
         jScrollPane.setBorder(null);
         jScrollPane.setBackground(pastelYellow);
+        centerPanel.setBackground(pastelYellow);
         for (int i = 0; i < paneList.size(); i++) {
             centerPanel.add(paneList.get(i));   // panel에 index를 줘서 변수를 주듯 이름을 매김
         }
@@ -37,10 +41,11 @@ public class ReportPage extends JPanel {
             JPanel newPane = new JPanel(null);
             btnList.add(new JButton(cardDtoList.get(dataIdx).getImage())); // carddatalist클래스의 이미지를 끌고와서 버튼에 넣어줌 근데 12번째 이미지 부터 넣어줌?
             lblList.add(new JLabel(cardDtoList.get(dataIdx).getTitle()));  // carddatalist클래스의 이미지를 끌고와서 버튼에 넣어줌 근데 12번째 이미지 부터 넣어줌?
-            btnList.get(i).setBounds(0 , 0, 200, 120);   // 위치는 따로 지정 해주지 않고 크기만 지정 해줌
-            lblList.get(i).setBounds(0, 120, 200, 20);  // 위치는 따로 지정 해주지 않고 크기만 지정 해줌
+            btnList.get(i).setBounds(60, 0, 150, 120);   // 위치는 따로 지정 해주지 않고 크기만 지정 해줌
+            lblList.get(i).setBounds(60, 120, 150, 20);  // 위치는 따로 지정 해주지 않고 크기만 지정 해줌
             newPane.add(btnList.get(i));
             newPane.add(lblList.get(i));
+            newPane.setBackground(pastelYellow);
             paneList.add(newPane);
         }
 
