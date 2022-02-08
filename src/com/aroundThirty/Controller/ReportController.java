885958d22619148365ddb95e74c4cc14e82ddd88
ood12 <ood1208@gmail.com>
2022-02-08 16:25:09 +0900

    Sol Ver1.4.3
    다운로드 받은 이미지 arr 삭제
    페이징 처리 수정(ReportPage) / 복사해서 쓰시면 됩니다.
    ㄴ그에 따른 FR,BR 등 수정

--- src/com/aroundThirty/Controller/ReportController.java
+++ src/com/aroundThirty/Controller/ReportController.java
@@ -1,4 +1,22 @@
 package com.aroundThirty.Controller;
 
+import com.aroundThirty.model.ReportDao;
+import com.aroundThirty.model.ReportDto;
+import com.aroundThirty.Resource.BR;
+
+
 public class ReportController {
+
+    public ReportController() {
+        BR.reportListAll = ReportDao.reportSelectAll();
+        for(ReportDto reportDto : BR.reportListAll) {
+            BR.reportDto = reportDto;
+        }
+    }
+
+//    public static void main(String[] args) {
+//        new ReportController();
+//    }
+
+
 }
--- src/com/aroundThirty/Resource/BR.java
+++ src/com/aroundThirty/Resource/BR.java
@@ -2,6 +2,9 @@ package com.aroundThirty.Resource;
 
 import com.aroundThirty.model.ReportDto;
 
+import java.util.List;
+
 public class BR {
     public static ReportDto reportDto;
+    public static List<ReportDto> reportListAll;
 }
